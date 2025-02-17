5f3a2c1cb2c47dd783635152ff013e96512027f8
Tim Häggqvist Luotomäki <tim@MBP16.local>
2025-02-17 17:31:20 +0100

    worked on day 5 state, some UI

--- src/components/days/DayFive.jsx
+++ src/components/days/DayFive.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { updateFieldDayFive, calculateDailyScoreDayFive, resetStateDayFive } from '../../redux/slices'
 import { FormField, PreviousEventScore, FormButtons, FormHeader } from '../form'
-import { DAY_KEYS } from '../../utils'
+import { DAY_KEYS, TROOP_TIER_MULTIPLIERS } from '../../utils'
 
 const DayFive = () => {
 
@@ -31,6 +31,56 @@ const DayFive = () => {
             <div className='flex flex-col md:flex-row md:pr-2'>
               <div className='w-full md:w-1/2 relative md:border-r border-neutral-400 md:pr-2'>
                 {/* Input */}
+                <div className='flex flex-col sm:flex-row space-x-2 px-1'>
+                  {/* Base troop tier */}
+                  <div className='w-full'>
+                    <label htmlFor='tribeLevel' className='block font-bold text-blue-900 mt-2'>Base Troop Tier:</label>
+                    <div className='relative w-full'>
+                      <select
+                        id="baseTier"
+                        className='w-full mt-1 px-1 border border-neutral-300 rounded-md shadow-sm appearance-none'
+                        value={dayFiveData.troopBaseTier}
+                        // onChange={(e) => setSelectedDropdownOption(Number(e.target.value))} // ensure selected value remains a number 
+                        onChange={(e) => handleInput('baseTroopTier', e.target.value)}
+                      >
+                        {Object.keys(TROOP_TIER_MULTIPLIERS).map((level, index) => (
+                          <option key={index} value={TROOP_TIER_MULTIPLIERS[level]}>
+                            {level}
+                          </option>
+                        ))}
+                      </select>
+                      <div className='absolute inset-y-0 right-2 flex items-center pointer-events-none'>
+                        <svg className='w-5 h-5 text-black' fill='none' stroke='black' strokeWidth='2' viewBox='0 0 24 24' >
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+                  {/* Target troop tier */}
+                  <div className='w-full'>
+                    <label htmlFor='tribeLevel' className='block font-bold text-blue-900 mt-2'>Target Troop Tier:</label>
+                    <div className='relative w-full'>
+                      <select
+                        id="tribeLevel"
+                        className='w-full mt-1 px-1 border border-neutral-300 rounded-md shadow-sm appearance-none'
+                        value={dayFiveData.troopTargetTier}
+                        // onChange={(e) => setSelectedDropdownOption(Number(e.target.value))} // ensure selected value remains a number 
+                        onChange={(e) => handleInput('targetTroopTier', e.target.value)}
+                      >
+                        {Object.keys(TROOP_TIER_MULTIPLIERS).map((level, index) => (
+                          <option key={index} value={TROOP_TIER_MULTIPLIERS[level]}>
+                            {level}
+                          </option>
+                        ))}
+                      </select>
+                      <div className='absolute inset-y-0 right-2 flex items-center pointer-events-none'>
+                        <svg className='w-5 h-5 text-black' fill='none' stroke='black' strokeWidth='2' viewBox='0 0 24 24' >
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+                </div>
                 <PreviousEventScore dayKey={DAY_KEYS.DAY_FIVE} />
               </div>
               <div className='w-full md:w-1/2 relative'>
--- src/redux/slices/dayFiveSlice.js
+++ src/redux/slices/dayFiveSlice.js
@@ -1,6 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { POINTS_AND_MULTIPLIERS, TROOP_TIER_MULTIPLIERS, loadData, saveData, cleanNumericValue } from "../../utils";
 
-const initialState = {
+
+const troopTierOptions = Object.keys(TROOP_TIER_MULTIPLIERS);
+
+const savedState = loadData();
+
+const initialState = savedState?.dayFive || {
+    troopBaseTier: TROOP_TIER_MULTIPLIERS[troopTierOptions[0]],
+    troopTargetTier: TROOP_TIER_MULTIPLIERS[troopTierOptions[troopTierOptions.length -1]],
+    promotedTroopPerBatch: '',
+    trainingTime: '',
+    trainingSpeedup: '',
     dailyScore: '',
     previousEventScore: {
         topOne: '',
@@ -14,22 +25,41 @@ const dayFiveSlice = createSlice({
     reducers: {
         updateField: (state, action) => {
             const { field, value } = action.payload;
-            // Numeric value validation
-            const cleanedValue = typeof value === 'string' ? value.replace(/[^0-9]/g, "") : value;
-            const numericValue = Number(cleanedValue);
 
             // Update previous event scores if the field belongs there
             if (field in state.previousEventScore) {
-                state.previousEventScore[field] = numericValue;
+                state.previousEventScore[field] = cleanNumericValue(value);
             } else {
-                state[field] = numericValue;
+                state[field] = cleanNumericValue(value);
             }
         },
-        calculateDailyScore: (state, action) => {
+        calculateDailyScore: (state) => {
+            // promoted tier - base tier = score multiplier
+            // troop number per training session 
+            // calculate number of upgrade sessions available 
+            // 
+            const scorePerTroop = POINTS_AND_MULTIPLIERS.LEVEL_GAP_MULTIPLIER + (state.troopTargetTier - state.troopBaseTier);
+            
+            const trainingBatches = Math.floor(state.trainingSpeedup / state.trainingTime);
+            
+            state.dailyScore = scorePerTroop * trainingBatches * state.promotedTroopPerBatch; 
+
 
         },
         resetState: (state) => {
+            state.troopBaseTier = TROOP_TIER_MULTIPLIERS[troopTierOptions[0]];
+            state.troopTargetTier = TROOP_TIER_MULTIPLIERS[troopTierOptions[troopTierOptions.length -1]];
+            state.promotedTroopPerBatch = '';
+            state.trainingTime = '';
+            state.trainingSpeedup = '';
+            state.dailyScore = '';
+            state.previousEventScore = {
+                topOne: '',
+                topTen: '',
+            };
 
+
+            saveData({ ...loadData(), dayFive: { ...state } });
         }
     }
 })
