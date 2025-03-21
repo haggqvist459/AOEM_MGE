8cd67405dfeb4e184cb65e138766ad65d94a6ff7
haggqvist459 <48819082+haggqvist459@users.noreply.github.com>
2025-03-21 15:09:18 +0100

    added vite PWA

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,30 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'icons/icon-512x512.png'],
+      manifest: {
+        name: 'M.G.E. Calc',
+        short_name: 'M.G.E.',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#172554',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
   ],
   server: {
     host: true,
